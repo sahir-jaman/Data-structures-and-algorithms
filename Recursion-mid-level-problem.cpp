7b384537eb924a3e8fc6a38a3aff22ccf3cf533a
sahir jaman <55667189+sahir-jaman@users.noreply.github.com>
2022-07-29 20:15:18 +0600

    input->432 as int; output -> four three two

---
+++ Recursion-mid-level-problem.cpp
@@ -0,0 +1,35 @@
+// if i give u 432 digit;
+// you should give me four,three,two as output; 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void digitRecursion(int);
+void printDigit(int);
+
+void digitRecursion(int n)
+{
+    if(n==0)
+    {
+        return;
+    }
+
+    digitRecursion(n/10);
+    //cout<<n%10<<" ";
+    printDigit(n%10);
+
+}
+
+void printDigit(int p){
+
+    string str[10] = {"ZERO","ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE"};
+    cout<<str[p]<<" ";
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    digitRecursion(n);
+}
